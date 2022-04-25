f43f2bf67cf2b10169c6c0b8d59070dfc0ca4e4e
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-04-24 23:34:29 -0500

    Map additions

--- direction.js
+++ direction.js
@@ -1,17 +1,25 @@
 const character = newImage('./images/Cat-character/static.gif')
 
+let lastDirection
+
 function handleDirectionChange(direction){
     if(direction === null){
-        character.src = `./images/Cat-character/static.gif`
+        if (lastDirection === 'west') {
+            character.src = `./images/Cat-character/static.gif`    
+        }  else  {
+            character.src = `./images/Cat-character/static-right.gif`
+        }
     }
     if(direction === 'west'){
         character.src = `./images/Cat-character/west.gif`
+        lastDirection = 'west'
     }
     if(direction === 'north'){
         character.src = `./images/Cat-character/north.gif`
     }
     if(direction === 'east'){
         character.src = `./images/Cat-character/east.gif`
+        lastDirection = 'east'
     }
     if(direction === 'south'){
         character.src = `./images/Cat-character/south.gif`
